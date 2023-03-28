f35011f58a5d960d68eb3fbba0384dc240ee02e1
widyafebriandaru <76604668+widyafebriandaru@users.noreply.github.com>
2023-03-28 17:13:51 +0700

    adding many responsive design, padding, margin, and new icon

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['index.html'],
+  theme: {
+    extend: {
+      colors: {
+        'warna1': '#E5FFDE',
+        'warna2': '#BBCBCB',
+        'warna3': '#9590AB',
+        'warna4': '#634B66',
+        'warna5': '#18020C'
+      }
+    },
+  },
+  plugins: [],
+}
